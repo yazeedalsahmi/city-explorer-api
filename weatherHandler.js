1e858e0b35ce0547b0834462444d7fa81e63229e
yazeed alshami <alshamiyazeed@gmail.com>
2021-08-12 04:08:40 +0300

    lab10

--- weatherHandler.js
+++ weatherHandler.js
@@ -2,24 +2,30 @@ const axios = require('axios');
 require('dotenv').config;
 const Weather = require('./Weather');
 
+let weatherMomery = {};
 let weatherHandler = (req, res) => {
 
-  const { latitude, longitude } = req.query;
+  let searchQuery = req.query.searchQuery;
+  if (weatherMomery[searchQuery] !== undefined) {
+    res.send(weatherMomery[searchQuery]);
 
-  //let searchQuery = req.query.searchQuery;
-  const weatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${latitude}&lon=${longitude}`;
-  axios.get(weatherURL).then(locWeatherValue => {
-    console.log(locWeatherValue.data.data);
-    let forcastArray = locWeatherValue.data.data.map((item) => {
-      return new Weather(item);
-    });
-    console.log(forcastArray);
-    res.json(forcastArray);
-    res.send(forcastArray);
-  })
-    .catch(err => {
-      res.send(err.message);
-    });
+
+  } else {
+    //let searchQuery = req.query.searchQuery;
+    const weatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${process.env.WEATHER_API_KEY}`;
+    axios.get(weatherURL).then(locWeatherValue => {
+      console.log(locWeatherValue.data.data);
+      let forcastArray = locWeatherValue.data.data.map((item) => {
+        return new Weather(item);
+      });
+      weatherMomery[searchQuery] = forcastArray;
+      console.log(forcastArray);
+      res.send(forcastArray);
+    })
+      .catch(err => {
+        res.send(err.message);
+      });
+  }
 };
-module.exports=weatherHandler;
+module.exports = weatherHandler;
 
